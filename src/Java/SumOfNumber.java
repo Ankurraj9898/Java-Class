efa0b53fdd3adf865d565abeed33a69aca58a789
Ankurraj9898 <ankurrajput47@gmail.com>
2026-03-29 22:56:03 +0530

    Add Print mmessga only

--- src/Java/SumOfNumber.java
+++ src/Java/SumOfNumber.java
@@ -5,22 +5,22 @@ public class SumOfNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		
-//		int num = 343553;
-//		
-//		int temp;
-//		int res = 0;
-//		
-//		while(num>0) {
-//			 
-//			temp= num%10;
-//			res=res+temp;
-//			num=num/10;
-//		}
-//		
-//		System.out.println(res);
 		
-	
+		int num = 343553;
+		
+		int temp;
+		int res = 0;
+		
+		while(num>0) {
+			 
+			temp= num%10;
+			res=res+temp;
+			num=num/10;
+		}
+		
+		System.out.println(res);
+		
+	System.out.println("HEllo Word");
 	}
 
 }
